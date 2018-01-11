106527fd6da648ecd8629f4e0df697cee7a58891
erikvw <ew2789@gmail.com>
2018-01-11 17:23:16 +0200

    rename helper func for clarity

--- ambition_metadata_rules/predicates.py
+++ ambition_metadata_rules/predicates.py
@@ -9,12 +9,12 @@ class Predicates(PredicateCollection):
     app_label = 'ambition_subject'
     visit_model = f'{app_label}.subjectvisit'
 
-    def check_gt_3_months(self, visit=None, panel_name=None):
+    def datetime_gt_3_months(self, visit=None, field=None):
         values = self.exists(
             reference_name=f'{self.app_label}.patienthistory',
             subject_identifier=visit.subject_identifier,
             report_datetime=visit.report_datetime,
-            field_name=panel_name)
+            field_name=field)
         return ((visit.report_datetime - relativedelta(months=3)).date() >
                 (values[0] or (visit.report_datetime).date()))
 
@@ -44,12 +44,12 @@ class Predicates(PredicateCollection):
 
     def func_require_cd4(self, visit, **kwargs):
         if visit.visit_code == DAY1:
-            return self.check_gt_3_months(
-                visit=visit, panel_name='cd4_date')
+            return self.datetime_gt_3_months(
+                visit=visit, field='cd4_date')
         return False
 
     def func_require_vl(self, visit, **kwargs):
         if visit.visit_code == DAY1:
-            return self.check_gt_3_months(
-                visit=visit, panel_name='viral_load_date')
+            return self.datetime_gt_3_months(
+                visit=visit, field='viral_load_date')
         return False
